85eff539a5a0a6897f7b9c2ed108178c23f7d94e
Todd Suomela <todd@halley.local>
2024-10-06 16:58:16 -0400

    remove custom and evil-dvorak

--- doom/dot-config/doom/packages.el
+++ doom/dot-config/doom/packages.el
@@ -11,7 +11,7 @@
 
 (package! org-modern)
 (package! org-superstar)
-(package! evil-dvorak)
+;;  (package! evil-dvorak)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
